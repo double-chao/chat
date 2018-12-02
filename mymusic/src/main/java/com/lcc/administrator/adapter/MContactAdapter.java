50ec557f87cd7f3f288c911e496061edab2c4671
Administrator <985208503@qq.com>
2018-12-02 15:08:30 +0800

    new class

--- mymusic/src/main/java/com/lcc/administrator/adapter/MContactAdapter.java
+++ mymusic/src/main/java/com/lcc/administrator/adapter/MContactAdapter.java
@@ -1,4 +1,9 @@
 package com.lcc.administrator.adapter;
 
+import com.lcc.administrator.vo.MContact;
+
+import java.util.List;
+
 public class MContactAdapter {
+    private List<MContact> mContactList;
 }
